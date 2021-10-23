777a8704452dd8c7163294948be96229e8a4585c
Akshay Rajguru <akshaygururaj141@gmail.com>
2021-10-24 00:14:57 +0530

    added webapps data and cosmetic changes

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=200, null=True)
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

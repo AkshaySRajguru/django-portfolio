efd393524d803eb4b5c445d4154656b66e15459e
Akshay Rajguru <akshaygururaj141@gmail.com>
2021-10-24 15:36:20 +0530

    deploy django to heroku

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,10 +19,17 @@ import jobs.views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# for deployment
+from django.conf.urls import url
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
     path('webapp/<int:job_id>', jobs.views.detail, name='detail'),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
